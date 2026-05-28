18102a40fe3d4be7477f8301429610c867ac131f
Meghana033 <meghanamadireddy7@gmail.com>
2026-05-28 16:11:20 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import baseJs from "@eslint/js";
+import browserGlobals from "globals";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import refreshPlugin from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
-    },
+    ignores: ["dist", "build"]
   },
-])
+
+  {
+    files: ["src/**/*.{js,jsx}"],
+
+    extends: [
+      baseJs.configs.recommended,
+      hooksPlugin.configs.flat.recommended,
+      refreshPlugin.configs.vite
+    ],
+
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+
+      globals: {
+        ...browserGlobals.browser
+      }
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    }
+  }
+]);
